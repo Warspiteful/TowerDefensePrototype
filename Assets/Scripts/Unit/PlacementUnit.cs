27b829a46871ef715b4433a26d5420751055042c
Warspiteful <jalmendral24@gmail.com>
2022-11-17 03:53:04 -0500

    Set up Directional Selection with Input Position
    Need to Work on Cancellation

--- Assets/Scripts/Unit/PlacementUnit.cs
+++ Assets/Scripts/Unit/PlacementUnit.cs
@@ -2,11 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//TODO: Make this a pooled object
 [RequireComponent(typeof(UnitInput))]
 public class PlacementUnit : MonoBehaviour
 {
+    private UnitInput _input;
+
+    [SerializeField] private Direction dir;
+    
     // Start is called before the first frame update
     void Start()
+    {
+        _input = GetComponent<UnitInput>();
+        _input.RegisterMousePositionCallback(CallbackType.DRAG, ChooseDirection);
+        _input.RegisterOnElsewhereClickCallback(Cancel);
+    }
+
+    public void Cancel()
+    {
+        Debug.Log("Cancel");
+    }
+
+    public void ChooseDirection(Vector3 direction)
+    {
+        Debug.Log(direction.x + ", " + direction.y + ", " + direction.z) ;
+        float x = direction.x;
+        float y = direction.y + direction.z;
+        if (x > 0 && Mathf.Abs(x) > Mathf.Abs(y))
+        {
+            Debug.Log("Direction Right");
+            dir = Direction.RIGHT;
+        }
+        else if (x < 0 && Mathf.Abs(x) > Mathf.Abs(y))
+        {
+            Debug.Log("Direction Left");
+            dir = Direction.LEFT;
+        }
+        
+        else if (y > 0 && Mathf.Abs(y) > Mathf.Abs(x))
+        {
+            Debug.Log("Direction Up");
+            dir = Direction.UP;
+
+        }
+        else if (y < 0 && Mathf.Abs(y) > Mathf.Abs(x))
+        {
+            Debug.Log("Direction Down");
+             dir = Direction.DOWN;
+        }
+        else
+        {
+            Debug.Log("NO DIRECTION");
+        }
+    }
+    public void Complete()
     {
         
     }
--- Assets/Scripts/Unit/UnitInput.cs
+++ Assets/Scripts/Unit/UnitInput.cs
@@ -11,24 +11,31 @@ public enum CallbackType{
 }
 
 
-public class UnitInput : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+public class UnitInput : MonoBehaviour
 {
     private PlayerControls _controls;
     
     [SerializeField] private LayerMask inputLayerMask;
+
+    [SerializeField] private bool isDragging;
     
     private void Start()
     {
         Camera.main.eventMask = inputLayerMask;
         _controls = new PlayerControls();
         _controls.Gameplay.Enable();
-        _controls.Gameplay.OnClick.performed += ctx => Onclick(ctx);
+        _controls.Gameplay.OnClick.started += ctx => OnClick(ctx);
+        _controls.Gameplay.OnClick.performed += ctx => OnClick(ctx);
+        _controls.Gameplay.OnClick.canceled += ctx => OnClick(ctx);
+
     }
 
     private Vector3Callback OnDragVector3Callback;
     private Vector3Callback OnDragBeginVector3Callback;
     private Vector3Callback OnDragEndVector3Callback;
     private VoidCallback OnClickVoidCallback;
+    private VoidCallback OnClickElsewhereVoidCallback;
+
 
     public void RegisterMousePositionCallback(CallbackType _callbackType, params Vector3Callback[] callback)
     {
@@ -36,22 +43,27 @@ public class UnitInput : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDra
         switch (_callbackType)
         {
             case CallbackType.DRAG:
-                _callback = OnDragVector3Callback;
+                foreach (Vector3Callback vector3Callback in callback)
+                {
+                    OnDragVector3Callback += vector3Callback;
+                }
                 break;
             case CallbackType.BEGINDRAG:
-                _callback = OnDragBeginVector3Callback;
+                foreach (Vector3Callback vector3Callback in callback)
+                {
+                    OnDragBeginVector3Callback += vector3Callback;
+                }
                 break;
             case CallbackType.ENDDRAG:
-                _callback = OnDragEndVector3Callback;
-                break;
+                foreach (Vector3Callback vector3Callback in callback)
+                {
+                    OnDragEndVector3Callback += vector3Callback;
+                }                break;
             default:
                 throw new ArgumentException("Invalid callback Type");
         }
 
-        foreach (Vector3Callback vector3Callback in callback)
-        {
-            _callback += vector3Callback;
-        }
+
     }
 
     public void RegisterOnClickCallback(params VoidCallback[] callback)
@@ -61,36 +73,64 @@ public class UnitInput : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDra
             OnClickVoidCallback += _callback;
         }
     }
+    
+    public void RegisterOnElsewhereClickCallback(params VoidCallback[] callback)
+    {
+        foreach(VoidCallback _callback in callback)
+        {
+            OnClickElsewhereVoidCallback += _callback;
+        }
+    }
 
-    public void Onclick(InputAction.CallbackContext ctx)
+    public void OnClick(InputAction.CallbackContext ctx)
     {
         Camera c = Camera.main;
         RaycastHit rcHit;
 
-        if (Physics.Raycast (Camera.main.ScreenPointToRay(
-                    Mouse.current.position.ReadValue()), out rcHit, Mathf.Infinity, inputLayerMask))
-        {
-            Debug.Log(rcHit.collider.gameObject.name);
-            OnClickVoidCallback?.Invoke();
 
+        Vector3 position = ctx.ReadValue<Vector2>();
+        
+        Ray ray = Camera.main.ScreenPointToRay(position);
+        Plane plane = new Plane(new Vector3(0,Mathf.Cos(Mathf.Deg2Rad*45), Mathf.Sin(-Mathf.Deg2Rad*45)), Vector3.zero);
+
+        float distance;
+        
+        if (plane.Raycast(ray, out distance))
+        {
+            Vector3 target = ray.GetPoint(distance);
+            position = target - transform.position;
+            position.Normalize();
+            position.y += position.z;
+        }
+   
+        if (ctx.started)
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(
+                    Mouse.current.position.ReadValue()), out rcHit, Mathf.Infinity, inputLayerMask))
+            {
+         
+                    OnClickVoidCallback?.Invoke();
+                    OnDragBeginVector3Callback?.Invoke(position);
+                    isDragging = true;
+                
+            }
+            else
+            {
+                OnClickElsewhereVoidCallback?.Invoke();
+            }
         }
         
+        else if (ctx.performed && isDragging)
+        {
+            OnDragVector3Callback?.Invoke(position);
+            Debug.Log("Dragging");
+        }
+        else if (ctx.canceled)
+        {
+            OnDragEndVector3Callback?.Invoke(position);
+            isDragging = false;
+        }
+       
 
-    
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        OnDragVector3Callback?.Invoke(Input.mousePosition);
-    }
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        OnDragBeginVector3Callback?.Invoke(Input.mousePosition);
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        OnDragEndVector3Callback?.Invoke(Input.mousePosition);
     }
 }
