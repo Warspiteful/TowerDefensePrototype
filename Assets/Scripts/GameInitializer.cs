a9ed780854e55f5252b10ca52fd0d803931a9a5d
Justin Almendral <jalmendral24@gmail.com>
2022-11-24 13:00:07 -0500

    Revert "Created Rudimentary End Screens"

--- Assets/Scripts/GameInitializer.cs
+++ Assets/Scripts/GameInitializer.cs
@@ -6,11 +6,9 @@ using UnityEngine;
 public class GameInitializer : MonoBehaviour
 {
     [SerializeField] private IntVariable lives;
-    [SerializeField] private int startingLives;
-
 
     private void Awake()
     {
-        lives.Value = startingLives;
+        lives.Value = 5;
     }
 }
