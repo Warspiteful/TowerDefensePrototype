357660ea22b5a93e9848df634c022b432b9a1fb5
Warspiteful <jalmendral24@gmail.com>
2023-01-14 02:10:42 -0500

    Began Work on Overlay

---
+++ Assets/Scripts/UI/DeployedUnitOverlay/DeployedUnitPanel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeployedUnitPanel : MonoBehaviour
+{
+    [SerializeField] private Image ArchtypeImage;
+    [SerializeField] private TextMeshProUGUI UnitName;
+    [SerializeField] private TextMeshProUGUI attackState;
+    [SerializeField] private TextMeshProUGUI defenseState;
+    [SerializeField] private TextMeshProUGUI blockNumber;
+    [SerializeField] private TextMeshProUGUI healthBar;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
